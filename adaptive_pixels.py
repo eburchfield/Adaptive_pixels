2cd510a8737cf6e3d2c448c25ea71bd50571bfa7
eburchfield <emilykayburchfield@gmail.com>
2017-04-25 16:39:17 -0500

    initial commit

---
+++ adaptive_pixels.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 25 16:27:24 2017
+
+@author: Emily Burchfield, emily.k.burchfield@vanderbilt.edu
+"""
+# INSTALL
+### sudo pip install pyModis, gdalm WxPython
+## GDAL
+## PyMODIS
+
+
+
+
+import os
+os.chdir('/data/emily/System_B')
+import agpredict as ap
+
